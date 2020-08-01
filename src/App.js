6cac193cfbdc49cf6778717e94a72a119e3d88f2
amacha2626 <amacha2626@gmail.com>
2020-08-02 01:27:58 +0900

    add youtube api

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React, { Component } from 'react';
 import './App.css';
-import Header from './components/Header/Header'
+import Header from './components/Header/Header';
+import YSearch from 'youtube-api-search';
+
+const YOUTUBE_API_KEY = 'AIzaSyDJ6TZNvsrxgvil9VQToLOD-xT2IYYCLGk'
 
 class App extends Component {
+  state = { videos: [] }
+
+  componentDidMount(){
+    YSearch({ key: YOUTUBE_API_KEY, term: 'にじさんじ' }, (data) => {
+      this.setState({ videos: data})
+    })
+  }
+
   render() {
+    console.log(this.state.videos)
     return (
       <div className="App">
         <Header />
